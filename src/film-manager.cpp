b2bb5c6b73ee1dd70b33bfb54a6685d98a219bed
Tyler Anderson <tasonchou@gmail.com>
2021-07-19 01:57:48 +0000

    Added film-manager.cpp and .gitignore

---
+++ src/film-manager.cpp
@@ -0,0 +1,15 @@
+//===-- film-manager.cpp - main application src file ----------------------===//
+//
+// Part of film-manager project, Copyright 2021 Tyler J. Anderson
+// This software is released under the BSD 3-Clause "New" or "Revised" License
+// You should have received a copy of the license with this source distribution
+// 
+// SPDX-License-Identifier: BSD-3-Clause
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the top-level application implementation for the film
+/// manager front-end program.
+///
+//===----------------------------------------------------------------------===//
